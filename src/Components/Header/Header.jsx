3eddd67a4b091bb3d91a68e175e570f2cee8cd83
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-07-20 08:04:59 +0300

    init

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./Header.css";
+function Header() {
+    const [toggle,showMenu]=useState(false);
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="" className="nav-logo ">
+          KEMO
+        </a>
+        <div className={toggle?"nav-menu show-menu":"nav-menu"}>
+          <ul className="nav-list grid">
+            <li className="nav-item">
+              <a href="#home" className="nav-link">
+                <i className="uil uil-estate nav-icon active-link"></i> Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#about" className="nav-link">
+                <i className="uil uil-user nav-icon "></i> About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="Skills" className="nav-link">
+                <i className="uil uil-file-alt nav-icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#services" className="nav-link">
+                <i className="uil uil-briefcase nav-icon"></i> Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#portfolio" className="nav-link">
+                <i className="uil uil-scenery nav-icon"></i> Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-link">
+                <i className="uil uil-message nav-icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+          <i className="uil uil-times nav-close" onClick={()=>showMenu(!toggle)}></i>
+        </div>
+          <div className="nav-toggle" onClick={()=>showMenu(!toggle)}>
+            <i className="uil uil-apps"></i>
+          </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
