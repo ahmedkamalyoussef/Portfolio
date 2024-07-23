314abae04ed19e2f47ad7f4506ca8d37d6e39846
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-07-23 08:06:29 +0300

    edit home css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import './Home.css'
-import Social from './Social'
-import Data from './Data'
-import ScrollDown from './ScrollDown'
+import React, { useEffect } from 'react';
+import './Home.css';
+import Social from './Social';
+import Data from './Data';
+import ScrollDown from './ScrollDown';
+
 function Home() {
+  useEffect(() => {
+    const img = document.querySelector('.home-img');
+    img.classList.add('animate');
+  }, []);
+
   return (
-    <section className=" home section" id="home">
-        <div className="home-container container grid">
-            <div className="home-content grid">
-                <Social/>
-                <div className="home-img"></div>
-                <Data/>
-            </div>
-            <ScrollDown/>
+    <section className="home section" id="home">
+      <div className="home-container container grid">
+        <div className="home-content grid">
+          <Social />
+          <div className="home-img"></div>
+          <Data />
         </div>
+        <ScrollDown />
+      </div>
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
