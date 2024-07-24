45e343f5d8a1ac5c80d7058bc7236676b8f98069
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-07-24 05:15:09 +0300

    edit header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,38 +5,38 @@ function Header() {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="" className="nav-logo ">
+        <a href="https://ahmedkamalyoussef.netlify.app/" className="nav-logo ">
           Ahmed
         </a>
         <div className={toggle?"nav-menu show-menu":"nav-menu"}>
           <ul className="nav-list grid">
             <li className="nav-item">
-              <a href="#home" className="nav-link" >
+              <a href="#home" className="nav-link" onClick={()=>showMenu(!toggle)}>
                 <i className="uil uil-estate nav-icon active-link"></i> Home
               </a>
             </li>
             <li className="nav-item">
-              <a href="#about" className="nav-link">
+              <a href="#about" className="nav-link" onClick={()=>showMenu(!toggle)}>
                 <i className="uil uil-user nav-icon "></i> About
               </a>
             </li>
             <li className="nav-item">
-              <a href="#skills" className="nav-link">
+              <a href="#skills" className="nav-link" onClick={()=>showMenu(!toggle)}>
                 <i className="uil uil-file-alt nav-icon"></i> Skills
               </a>
             </li>
             <li className="nav-item">
-              <a href="#services" className="nav-link">
+              <a href="#services" className="nav-link" onClick={()=>showMenu(!toggle)}>
                 <i className="uil uil-briefcase nav-icon"></i> Services
               </a>
             </li>
             <li className="nav-item">
-              <a href="#portfolio" className="nav-link">
+              <a href="#portfolio" className="nav-link" onClick={()=>showMenu(!toggle)}>
                 <i className="uil uil-scenery nav-icon"></i> Portfolio
               </a>
             </li>
             <li className="nav-item">
-              <a href="#contact" className="nav-link">
+              <a href="#contact" className="nav-link" onClick={()=>showMenu(!toggle)}>
                 <i className="uil uil-message nav-icon"></i> Contact
               </a>
             </li>
