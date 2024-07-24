6159423136318ad21bb7e943ac01e26cfc2b354f
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-07-24 07:31:24 +0300

    delete build

--- src/Components/Work/WorkItems.jsx
+++ src/Components/Work/WorkItems.jsx
@@ -5,7 +5,7 @@ function WorkItems({item}) {
     <div className="work-card">
         <img src={item.img} alt="" className='work-img'/>
         <h3 className="work-title">{item.title}</h3>
-        {item.isHasDemo==true?
+        {item.isHasDemo===true?
         <a href={item.demo} className="work-button">
             Demo {" "} <i className="bx bx-right-arrow-alt work-button-icon"></i>
         </a>:""
